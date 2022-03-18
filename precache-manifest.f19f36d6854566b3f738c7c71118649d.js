aadeba4e023b321f3a0a52a3b380d2ac4b8942fc
EshharFriedman <friedmaneshhar@gmail.com>
2022-03-18 15:50:20 +0200

    Updates

---
+++ precache-manifest.f19f36d6854566b3f738c7c71118649d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8669bf8575e01b23f6ade822a8f4d488",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/emojipedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "001a2739467010043a22",
+    "url": "/emojipedia/static/js/main.e092c6e9.chunk.js"
+  },
+  {
+    "revision": "7ea935b83df6c53ba113",
+    "url": "/emojipedia/static/js/runtime-main.d52a7a0e.js"
+  }
+]);
